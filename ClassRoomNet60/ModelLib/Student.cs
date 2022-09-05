e21ce07d1fd65bc7c2f9c05bcc7052eb7f314186
Oliver Olskær <oliv320b@easjdk.onmicrosoft.com>
2022-09-05 14:36:51 +0200

    Student Class

---
+++ ClassRoomNet60/ModelLib/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60.ModelLib
+{
+    public class Student
+    {
+        private string _Name;
+        private int _Birth_month;
+        private int _Birthday;
+
+
+        public Student(string Name, int Birth_month, int Birthday)
+        {
+
+            _Name = Name;
+            _Birth_month = Birth_month;
+            _Birthday = Birthday;
+
+        }
+
+        public string Name
+        {
+            get => _Name;
+           
+        }
+
+        public int Birth_month
+        {
+            get => _Birth_month;
+        }
+
+        public int Birthday
+        {
+            get => _Birthday;
+        }
+    }
+}
